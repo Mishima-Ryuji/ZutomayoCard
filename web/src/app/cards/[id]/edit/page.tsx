7b3af2837f29b253dd252eb4e276b1d72ebb1293
三嶋 隆史 <ryuji.mishima.com@gmail.com>
2023-06-08 19:41:36 +0900

    🎨 FormのUXを改善

--- web/src/app/cards/[id]/edit/page.tsx
+++ web/src/app/cards/[id]/edit/page.tsx
@@ -3,6 +3,7 @@
 import {
   Button,
   FormControl,
+  FormHelperText,
   FormLabel,
   Icon,
   Input,
@@ -38,7 +39,7 @@ const FileUpload = ({ onChange: handleChange }: FileUploadProps) => {
   const handleClick = () => inputRef.current?.click()
 
   return (
-    <InputGroup onClick={handleClick}>
+    <InputGroup onClick={handleClick} mb={3}>
       <input
         type={'file'}
         multiple={false}
@@ -67,8 +68,9 @@ const Page = () => {
     register,
     reset,
     control,
+    watch,
     formState: { errors, isValid, isDirty },
-  } = useForm<Omit<Card, 'image' | DocumentBaseKey>>()
+  } = useForm<Omit<Card, 'image' | DocumentBaseKey>>({ mode: 'onBlur' })
 
   const onSubmit = handleSubmit(async (data) => {
     if (!card) return
@@ -76,6 +78,10 @@ const Page = () => {
     await updateDoc(card.ref, data)
     router.push('/')
   })
+
+  const cardData = watch()
+
+  console.log(cardData)
 
   useEffect(() => {
     if (!card) return
@@ -98,6 +104,9 @@ const Page = () => {
           <VStack width={'100%'} my={5} alignItems={'baseline'} gap={5}>
             <FormControl>
               <FormLabel htmlFor="file">画像</FormLabel>
+              <FormHelperText mb={3}>
+                公式の画像は使用が禁止されているため、自分で写真を撮ってアプロードしてください。
+              </FormHelperText>
               <FileUpload
                 onChange={async (e) => {
                   if (!card) return
@@ -118,8 +127,8 @@ const Page = () => {
                   })
                 }}
               />
+              <CardItem card={card} width={200} />
             </FormControl>
-            <CardItem card={card} width={200} />
             <FormControl>
               <FormLabel htmlFor="rarity">レアリティ</FormLabel>
               <Select
@@ -185,58 +194,67 @@ const Page = () => {
                 isInvalid={!!errors.name_furigana}
                 {...register('name_furigana', { required: true })}
               />
+              <FormHelperText>
+                英語はそのまま入力してください。例: DNA鍋 → DNAなべ
+              </FormHelperText>
             </FormControl>
             <FormControl>
               <FormLabel htmlFor="effect">効果</FormLabel>
               <Textarea
-                placeholder="カード名"
                 isInvalid={!!errors.effect}
                 {...register('effect', { required: true })}
               />
             </FormControl>
-            <FormControl>
-              <FormLabel htmlFor="day_offensive_strength">
-                攻撃力 (Day)
-              </FormLabel>
-              <Controller
-                name="day_offensive_strength"
-                control={control}
-                render={({ field: { onChange, value } }) => (
-                  <NumberInput
-                    isInvalid={!!errors.day_offensive_strength}
+            {cardData.type === 'character' && (
+              <>
+                <FormControl>
+                  <FormLabel htmlFor="day_offensive_strength">
+                    攻撃力 (Day)
+                  </FormLabel>
+                  <Controller
+                    rules={{ required: true }}
                     name="day_offensive_strength"
-                    onChange={onChange}
-                    value={value}
-                  >
-                    <NumberInputField />
-                  </NumberInput>
-                )}
-              />
-            </FormControl>
-            <FormControl>
-              <FormLabel htmlFor="night_offensive_strength">
-                攻撃力 (Night)
-              </FormLabel>
-              <Controller
-                name="night_offensive_strength"
-                control={control}
-                render={({ field: { onChange, value } }) => (
-                  <NumberInput
-                    isInvalid={!!errors.night_offensive_strength}
+                    control={control}
+                    render={({ field: { onChange, value } }) => (
+                      <NumberInput
+                        isInvalid={!!errors.day_offensive_strength}
+                        name="day_offensive_strength"
+                        onChange={onChange}
+                        value={value}
+                      >
+                        <NumberInputField />
+                      </NumberInput>
+                    )}
+                  />
+                </FormControl>
+                <FormControl>
+                  <FormLabel htmlFor="night_offensive_strength">
+                    攻撃力 (Night)
+                  </FormLabel>
+                  <Controller
                     name="night_offensive_strength"
-                    onChange={onChange}
-                    value={value}
-                  >
-                    <NumberInputField />
-                  </NumberInput>
-                )}
-              />
-            </FormControl>
+                    rules={{ required: true }}
+                    control={control}
+                    render={({ field: { onChange, value } }) => (
+                      <NumberInput
+                        isInvalid={!!errors.night_offensive_strength}
+                        name="night_offensive_strength"
+                        onChange={onChange}
+                        value={value}
+                      >
+                        <NumberInputField />
+                      </NumberInput>
+                    )}
+                  />
+                </FormControl>
+              </>
+            )}
             <FormControl>
               <FormLabel htmlFor="clock">時計</FormLabel>
               <Controller
                 name="clock"
                 control={control}
+                rules={{ required: true }}
                 render={({ field: { onChange, value } }) => (
                   <NumberInput
                     isInvalid={!!errors.clock}
@@ -253,6 +271,7 @@ const Page = () => {
               <FormLabel htmlFor="power_cost">POWER COST</FormLabel>
               <Controller
                 name="power_cost"
+                rules={{ required: true }}
                 control={control}
                 render={({ field: { onChange, value } }) => (
                   <NumberInput
@@ -265,11 +284,15 @@ const Page = () => {
                   </NumberInput>
                 )}
               />
+              <FormHelperText>
+                存在しない場合は0を入れてください。
+              </FormHelperText>
             </FormControl>
             <FormControl>
               <FormLabel htmlFor="power">SEND TO POWER</FormLabel>
               <Controller
                 name="power"
+                rules={{ required: true }}
                 control={control}
                 render={({ field: { onChange, value } }) => (
                   <NumberInput
@@ -282,12 +305,16 @@ const Page = () => {
                   </NumberInput>
                 )}
               />
+              <FormHelperText>
+                存在しない場合は0を入れてください。
+              </FormHelperText>
             </FormControl>
             <Button
               mt={4}
               colorScheme="purple"
               type="submit"
-              disabled={isSubmitting || !isValid || !isDirty}
+              isLoading={isSubmitting}
+              isDisabled={!isValid || !isDirty}
             >
               更新
             </Button>
