090196bf2ca3a49851da3a9bfab1b1506c2ca780
三嶋 隆史 <ryuji.mishima.com@gmail.com>
2023-07-21 04:41:31 +0900

    🎨 バナーの大きさを修正

--- web/src/components/InvitationBanners.tsx
+++ web/src/components/InvitationBanners.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 export const InvitationBanners = () => {
   return (
-    <AspectRatio width={'50%'} ratio={720 / 90}>
+    <AspectRatio width={['100%', '100%', '50%']} ratio={720 / 90}>
       <Image alt="招待バナー" src={'/invitation_banner.png'} fill />
     </AspectRatio>
   )
