4cdf1962d460f91b6ea592afbaefa8675f5d683e
TBSten <81161390+TBSten@users.noreply.github.com>
2023-08-07 01:15:09 +0900

    💄 表示テキストを設定

--- web/src/components/richText/ReactEditorJS.tsx
+++ web/src/components/richText/ReactEditorJS.tsx
@@ -16,11 +16,41 @@ export const tools: EditorConfig["tools"] = {
 }
 export const i18n: EditorConfig["i18n"] = {
   messages: {
+    ui: {
+      "blockTunes": {
+        "toggler": {
+          "Click to tune": "クリックして移動",
+          "or drag to move": "ドラッグして移動"
+        },
+      },
+      "toolbar": {
+        "toolbox": {
+          "Add": "行を追加"
+        }
+      }
+    },
     tools: {
-      "Add a link": "リンクを追加",
+      link: {
+        "Add a link": "リンクを追加",
+      },
     },
     toolNames: {
       "Text": "テキスト",
+      "Link": "URL",
+      "Bold": "太文字",
+      "Italic": "斜体",
+    },
+    blockTunes: {
+      "delete": {
+        "Delete": "行を削除",
+        "Click to delete": "削除してもいいですか？",
+      },
+      "moveUp": {
+        "Move up": "上に移動"
+      },
+      "moveDown": {
+        "Move down": "下に移動"
+      }
     },
   }
 }
