0738e6243ca4df6aff24b0721bf81c99f7ad2255
TBSten <81161390+TBSten@users.noreply.github.com>
2023-08-13 03:14:37 +0900

    ➕ lexicalを追加

--- web/src/components/richText/editor/RichEditor.tsx
+++ web/src/components/richText/editor/RichEditor.tsx
@@ -1,14 +1,33 @@
 
 import { OutputData } from "@editorjs/editorjs"
+import { LexicalComposer } from "@lexical/react/LexicalComposer"
+import { ContentEditable } from '@lexical/react/LexicalContentEditable'
+import LexicalErrorBoundary from '@lexical/react/LexicalErrorBoundary'
+import { HistoryPlugin } from '@lexical/react/LexicalHistoryPlugin'
+import { PlainTextPlugin } from '@lexical/react/LexicalPlainTextPlugin'
 import dynamic from "next/dynamic"
 import { FC, ReactNode } from "react"
 
 export interface RichEditorProps {
+  editorKey: string
   placeholder: ReactNode
 }
-const RichEditor: FC<RichEditorProps> = () => {
+const RichEditor: FC<RichEditorProps> = ({ editorKey, placeholder }) => {
+  const initialConfig = {
+    namespace: editorKey,
+    theme: {},
+    onError: console.error,
+  }
+
   return (
-    <></>
+    <LexicalComposer initialConfig={initialConfig}>
+      <PlainTextPlugin
+        contentEditable={<ContentEditable className="outline-none" />}
+        placeholder={<>{placeholder}</>}
+        ErrorBoundary={LexicalErrorBoundary}
+      />
+      <HistoryPlugin />
+    </LexicalComposer>
   )
 }
 
