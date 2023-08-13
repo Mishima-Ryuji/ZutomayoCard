baf0ac76ea96b68ee83fee1904452b961735179b
TBSten <81161390+TBSten@users.noreply.github.com>
2023-08-14 01:05:18 +0900

    ✨ getCurrentDataで現在の保存するデータを取得する機能を追加

---
+++ web/src/components/richText/editor/RefPlugin.tsx
@@ -0,0 +1,19 @@
+import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext"
+import { OnChangePlugin } from "@lexical/react/LexicalOnChangePlugin"
+import { EditorState } from "lexical"
+import { FC, MutableRefObject } from "react"
+
+interface RefPluginProps {
+  editorStateRef: MutableRefObject<EditorState | undefined>
+}
+const RefPlugin: FC<RefPluginProps> = ({ editorStateRef }) => {
+  const [editor] = useLexicalComposerContext()
+  editorStateRef.current = editor.getEditorState()
+  return (
+    <>
+      <OnChangePlugin onChange={editorState => { editorStateRef.current = editorState }} />
+    </>
+  )
+}
+
+export default RefPlugin
--- web/src/components/richText/editor/RichEditor.tsx
+++ web/src/components/richText/editor/RichEditor.tsx
@@ -1,23 +1,33 @@
 
 import { Divider, HStack } from "@chakra-ui/react"
-import { OutputData } from "@editorjs/editorjs"
 import { InitialConfigType, LexicalComposer } from "@lexical/react/LexicalComposer"
 import { ContentEditable } from '@lexical/react/LexicalContentEditable'
 import LexicalErrorBoundary from '@lexical/react/LexicalErrorBoundary'
 import { HistoryPlugin } from '@lexical/react/LexicalHistoryPlugin'
 import { RichTextPlugin as LexicalRichTextPlugin } from "@lexical/react/LexicalRichTextPlugin"
+import { EditorState, SerializedEditorState } from "lexical"
 import dynamic from "next/dynamic"
-import { FC, ReactNode } from "react"
+import { FC, MutableRefObject, ReactNode, useCallback, useRef } from "react"
 import { ZcwBoldToolbarItem } from "../ZcwBoldPlugin"
 import ZcwLinkPlugin, { ZcwLinkNode, ZcwLinkToolbarItem } from "../ZcwLinkPlugin"
 import styles from "./RichEditor.module.scss"
 
+import RefPlugin from "./RefPlugin"
+
 export interface RichEditorProps {
   editorKey: string
-  placeholder: ReactNode
+  placeholder?: ReactNode
+  initialState?: SerializedEditorState | null
+  editorStateRef: MutableRefObject<EditorState | undefined>
 }
-const RichEditor: FC<RichEditorProps> = ({ editorKey, placeholder }) => {
+const RichEditor: FC<RichEditorProps> = ({ editorKey, placeholder, initialState = null, editorStateRef, }) => {
   const initialConfig: InitialConfigType = {
+    editorState: initialState
+      ? editor => {
+        const parsedEditorState = editor.parseEditorState(initialState)
+        editor.setEditorState(parsedEditorState)
+      }
+      : null,
     namespace: editorKey,
     theme: {
       link: styles.link,
@@ -42,6 +52,7 @@ const RichEditor: FC<RichEditorProps> = ({ editorKey, placeholder }) => {
       />
       <HistoryPlugin />
       <ZcwLinkPlugin />
+      <RefPlugin editorStateRef={editorStateRef} />
     </LexicalComposer>
   )
 }
@@ -51,7 +62,21 @@ export default dynamic(async () => RichEditor, { ssr: false })
 
 interface UseRichEditorOptions {
   editorKey: string
-  defaultValue?: OutputData
+  defaultValue?: SerializedEditorState
 }
 export function useRichEditor({ editorKey, defaultValue }: UseRichEditorOptions) {
+  const editorStateRef = useRef<EditorState>()
+  const props = {
+    editorKey,
+    initialState: defaultValue,
+    editorStateRef,
+  } satisfies RichEditorProps
+  const getCurrentData = useCallback(() => {
+    return editorStateRef.current?.toJSON()
+  }, [])
+  return {
+    editorStateRef,
+    props,
+    getCurrentData,
+  }
 }
