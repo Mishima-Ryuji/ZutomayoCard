5722d92645058b98ecdafd000c392008b7f78ffe
TBSten <81161390+TBSten@users.noreply.github.com>
2023-08-14 15:24:51 +0900

    ✨ URL入力画面にカードを指定する機能を追加

--- web/src/components/richText/ZcwLinkPlugin.tsx
+++ web/src/components/richText/ZcwLinkPlugin.tsx
@@ -1,9 +1,12 @@
-import { AlertDialog, AlertDialogBody, AlertDialogContent, AlertDialogFooter, AlertDialogHeader, AlertDialogOverlay, Button, Input, useDisclosure } from '@chakra-ui/react'
+import { AlertDialog, AlertDialogBody, AlertDialogContent, AlertDialogFooter, AlertDialogHeader, AlertDialogOverlay, Box, Button, Heading, Input, List, Spinner, useDisclosure } from '@chakra-ui/react'
 import { $isLinkNode, LinkNode, TOGGLE_LINK_COMMAND } from '@lexical/link'
 import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext"
 import { LinkPlugin as LexicalLinkPlugin } from '@lexical/react/LexicalLinkPlugin'
 import { $getSelection, $isRangeSelection, COMMAND_PRIORITY_CRITICAL, SELECTION_CHANGE_COMMAND } from 'lexical'
-import { FC, useEffect, useRef, useState } from "react"
+import { FC, useEffect, useMemo, useRef, useState } from "react"
+import { useCollectionDataOnce } from 'react-firebase-hooks/firestore'
+import { cardsRef, getCategoryDetail } from '~/firebase'
+import { CardElementImage } from '../card/ElementImage'
 import { getSelectedNode } from './util/getSelectedNode'
 
 export const ZcwLinkNode = LinkNode
@@ -41,7 +44,6 @@ export const ZcwLinkToolbarItem: FC = () => {
   }, [editor])
 
   const inputUrlDialog = useDisclosure()
-  const inputUrlRef = useRef<HTMLInputElement>(null)
   const [url, setUrl] = useState("")
   const isValidUrl = url.trim().length >= 1
 
@@ -54,37 +56,107 @@ export const ZcwLinkToolbarItem: FC = () => {
     editor.dispatchCommand(TOGGLE_LINK_COMMAND, null)
     inputUrlDialog.onClose()
   }
+
   return (
     <>
       <Button variant={isLink ? "outline" : "ghost"} onClick={inputUrlDialog.onOpen} size="sm">
         🔗 リンク
       </Button>
-      <AlertDialog
+      <LinkInputDialog
         isOpen={inputUrlDialog.isOpen}
         onClose={inputUrlDialog.onClose}
-        leastDestructiveRef={inputUrlRef}
-      >
-        <AlertDialogOverlay>
-          <AlertDialogContent>
-            <AlertDialogHeader>
-              URLを入力
-            </AlertDialogHeader>
-            <AlertDialogBody>
-              <Input
-                value={url} onChange={e => setUrl(e.target.value)}
-              />
-            </AlertDialogBody>
-            <AlertDialogFooter>
-              <Button onClick={handleRemove}>
-                削除
-              </Button>
-              <Button onClick={handleAdd} colorScheme="purple" isDisabled={!isValidUrl}>
-                設定
-              </Button>
-            </AlertDialogFooter>
-          </AlertDialogContent>
-        </AlertDialogOverlay>
-      </AlertDialog>
+        url={url}
+        onChangeUrl={setUrl}
+        isValidUrl={isValidUrl}
+        onAdd={handleAdd}
+        onRemove={handleRemove}
+      />
     </>
+  )
+}
+
+interface LinkInputDialogProps {
+  isOpen: boolean
+  onClose: () => void
+  url: string
+  onChangeUrl: (url: string) => void
+  isValidUrl: boolean
+  onAdd: () => void
+  onRemove: () => void
+}
+const LinkInputDialog: FC<LinkInputDialogProps> = ({
+  isOpen, onClose,
+  url, onChangeUrl, isValidUrl,
+  onAdd, onRemove,
+}) => {
+  const [cards, loadingCards] = useCollectionDataOnce(cardsRef)
+  const sortedCards = useMemo(() =>
+    cards?.sort((a, b) => a.order - b.order),
+    [cards],
+  )
+
+  const inputUrlRef = useRef<HTMLInputElement>(null)
+
+  return (
+    <AlertDialog
+      isOpen={isOpen}
+      onClose={onClose}
+      leastDestructiveRef={inputUrlRef}
+    >
+      <AlertDialogOverlay>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            URLを入力
+          </AlertDialogHeader>
+          <AlertDialogBody>
+            <Input
+              value={url} onChange={e => onChangeUrl(e.target.value)}
+            />
+            <Heading size="xs">
+              カード
+            </Heading>
+            <List maxH="50vh" overflow="auto">
+              {loadingCards &&
+                <Spinner />
+              }
+              {sortedCards?.map(card => {
+                const categoryDetail = getCategoryDetail(card)
+                const cardUrl = `https://zutomayo-card.com/cards/${card.id}`
+                return (
+                  <Box
+                    key={card.id}
+                    as="li"
+                    onClick={() => onChangeUrl(cardUrl)}
+                    borderLeft={cardUrl === url ? "solid 2px" : "none"}
+                    borderColor="purple.300"
+                    display="flex" flexDir="row"
+                    justifyContent="flex-start" alignItems="center"
+                    cursor="pointer"
+                  >
+                    {card.element && (
+                      <CardElementImage element={card.element} size={20} />
+                    )}
+                    <Box flexGrow="1">
+                      {card.name}
+                    </Box>
+                    <Box textAlign="end" fontSize="0.8em">
+                      ({categoryDetail.name} {card.no}/{categoryDetail.denominator})
+                    </Box>
+                  </Box>
+                )
+              })}
+            </List>
+          </AlertDialogBody>
+          <AlertDialogFooter>
+            <Button onClick={onRemove}>
+              削除
+            </Button>
+            <Button onClick={onAdd} colorScheme="purple" isDisabled={!isValidUrl}>
+              設定
+            </Button>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialogOverlay>
+    </AlertDialog>
   )
 }
