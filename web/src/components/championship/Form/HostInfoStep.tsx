fa178e8680633d2cbe78d0cd096f3bb47dc063ac
TBSten <81161390+TBSten@users.noreply.github.com>
2023-09-10 23:25:02 +0900

    🚧 HostInfoStepを追加

---
+++ web/src/components/championship/Form/HostInfoStep.tsx
@@ -0,0 +1,84 @@
+import { Box, Input, Textarea } from "@chakra-ui/react"
+import { FC, useState } from "react"
+import { Championship } from "~/shared/firebase/firestore/scheme/championship"
+import { FormField } from "./FormField"
+import { FormStep } from "./FormStep"
+import { isValidAll } from "./util"
+import { ControlledFormField, InputStepTypes } from "./type"
+import { controlledFormFieldOf } from "./util"
+
+export type HostInfoInput = Pick<Championship,
+  | "host_uid"
+  | "host_name"
+  | "host_contact"
+>
+type HostInfoStepTypes = InputStepTypes<HostInfoInput>
+type HostInfoStepFields = {
+  host_uid: ControlledFormField<string>
+  host_name: ControlledFormField<string>
+  host_contact: ControlledFormField<string>
+}
+
+export const HostInfoStep: FC = () => {
+  return (
+    <FormStep
+      title="主催者情報"
+    />
+  )
+}
+
+interface InputHostInfoStepProps {
+  fields: HostInfoStepFields
+}
+export const InputHostInfoStep: FC<InputHostInfoStepProps> = ({
+  fields,
+}) => {
+  return (
+    <Box>
+      <FormField label="9. 主催者名">
+        <Input
+          value={fields.host_name.value}
+          onChange={e => fields.host_name.onChange(e.target.value)}
+          isInvalid={!fields.host_name.isValid}
+        />
+      </FormField>
+
+      <FormField label="10. 主催者の連絡先">
+        <Textarea
+          value={fields.host_contact.value}
+          onChange={e => fields.host_contact.onChange(e.target.value)}
+          isInvalid={!fields.host_contact.isValid}
+        />
+      </FormField>
+    </Box>
+  )
+}
+
+export const useHostInfoStep = ({ defaultValue }: HostInfoStepTypes["hookOptions"]) => {
+  const [hostUid, setHostUid] = useState(defaultValue.host_uid)
+  const [hostName, setHostName] = useState(defaultValue.host_name)
+  const [hostContact, setHostContact] = useState(defaultValue.host_contact)
+  const fields: HostInfoStepFields = {
+    host_uid: controlledFormFieldOf(
+      hostUid, setHostUid,
+      { isValid: hostUid !== "" }
+    ),
+    host_name: controlledFormFieldOf(
+      hostName, setHostName,
+      { isValid: hostName.trim() !== "" }
+    ),
+    host_contact: controlledFormFieldOf(
+      hostContact, setHostContact,
+      { isValid: hostContact.trim() !== "" }
+    ),
+  }
+  return {
+    fields,
+    isValid: isValidAll(fields),
+    props: {
+      input: {
+        fields,
+      },
+    },
+  }
+}
